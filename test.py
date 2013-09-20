491eb5bf42813aa34c57451d0bf0664e96319be6
Yak! <yak_ex@mx.scn.tv>
2013-09-20 17:00:48 +0900

    Dump result type and parameter types and names.

--- test.py
+++ test.py
@@ -14,10 +14,28 @@ def dump(level, node):
     for c in node.get_children():
         dump(level + 1, c)
 
+def dump_type(type):
+    """
+    """
+    result = "const " if type.is_const_qualified() else ""
+    if type.kind.name == "TYPEDEF":
+        result += type.get_declaration().spelling + '=(' + dump_type(type.get_declaration().underlying_typedef_type) + ')'
+    else:
+        result += type.kind.name
+        if type.kind.name == "POINTER":
+            result += " -> " + dump_type(type.get_pointee())
+    return result
+
 def dump_func(node):
     """
     """
     print '%s at %s' % (node.spelling, node.location.file)
+    print dump_type(node.type.get_result())
+    for at in node.type.argument_types():
+        print dump_type(at)
+    for p in node.get_children():
+        if p.kind.name == 'PARM_DECL':
+            print p.spelling
 
 index = clang.cindex.Index.create()
 tu = index.parse(sys.argv[1])
