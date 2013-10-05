78f97dac32ea959fdaa44277b040a6324f9d9eef
Yak! <yak_ex@mx.scn.tv>
2013-10-05 11:16:05 +0900

    Use a class object for function descriptor instead of a tuple.

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ import os.path
 
 # TODO: Avoid magic values
 
+
 def dump(level, node):
     """ 
     """
@@ -32,49 +33,85 @@ def dump_type(type):
     result += " volatile" if type.is_volatile_qualified() else ""
     return result
 
-def make_func_desc(node):
-    """
-    """
-    plist = zip([dump_type(at) for at in node.type.argument_types()], [p.spelling for p in node.get_children() if p.kind.name == 'PARM_DECL'])
-    return (node.spelling, node.location.file, dump_type(node.type.get_result()), plist)
+class FunctionDescriptor(object):
+    def __init__(self, node = None):
+        """Create function descriptor from libclang node"""
+        if node is None:
+            return
+        plist = zip([dump_type(at) for at in node.type.argument_types()], [p.spelling for p in node.get_children() if p.kind.name == 'PARM_DECL'])
+        self._name = node.spelling
+        self._file = node.location.file
+        self._result_type = dump_type(node.type.get_result())
+        self._parameter_types = plist
 
-def dump_func(desc):
-    """
-    """
-    print '%s %s(%s) at %s' % (desc[2], desc[0], ', '.join([t + ' ' + n for t, n in desc[3]]), desc[1])
+    def dump_func(self):
+        """
+        """
+        print '%s %s(%s) at %s' % (self._result_type, self._name, ', '.join([t + ' ' + n for t, n in self._parameter_types]), self._file)
 
-def make_func_decl(desc):
-    """
-    """
-    return '%s %s(%s)' % (desc[2], desc[0], ', '.join([t + ' ' + n for t, n in desc[3]]))
+    def make_func_decl(self):
+        """
+        """
+        return '%s %s(%s)' % (self._result_type, self._name, ', '.join([t + ' ' + n for t, n in self._parameter_types]))
 
-def make_func_call(desc):
-    """
-    """
-    return '%s(%s)' % (desc[0], ', '.join([n for t, n in desc[3]]))
+    def make_func_call(self):
+        """
+        """
+        return '%s(%s)' % (self._name, ', '.join([n for t, n in self._parameter_types]))
 
-def index_arg(desc, spec):
-    """
-    """
-    for i,v in enumerate(desc[3]):
-        if v == spec:
-            return i
-        elif v[0] == spec[0] and spec[1] == None:
-            return i
-        elif v[1] == spec[1] and spec[0] == None:
-            return i
-    return -1
+    def index_arg(self, spec):
+        """search argument corresponding to the specified spec"""
+        for i,v in enumerate(self._parameter_types):
+            if v == spec:
+                return i
+            elif v[0] == spec[0] and spec[1] == None:
+                return i
+            elif v[1] == spec[1] and spec[0] == None:
+                return i
+        return -1
+
+    def clone(self):
+        cloned = FunctionDescriptor()
+        cloned._name = self._name
+        cloned._file = self._file
+        cloned._result_type = self._result_type
+        cloned._parameter_types = self._parameter_types[:]
+        return cloned
+
+    def _get_name(self):
+        return self._name
+    def _set_name(self, name):
+        self._name = name
+    name = property(_get_name, _set_name)
+
+    def _get_file(self):
+        return self._file
+    def _set_file(self, file):
+        self._file = file
+    file = property(_get_file, _set_file)
+
+    def _get_result_type(self):
+        return self._result_type
+    def _set_result_type(self, result_type):
+        self._result_type = result_type
+    result_type = property(_get_result_type, _set_result_type)
+
+    def _get_parameter_types(self):
+        return self._parameter_types
+    def _set_parameter_types(self, parameter_types):
+        self._parameter_types = parameter_types
+    parameter_types = property(_get_parameter_types, _set_parameter_types)
 
 def read_only_wo_len(desc, spec):
     """
     """
-    target_index = index_arg(desc, spec[0])
-    orig = desc[3][target_index][1]
-    desc_self = (desc[0], desc[1], desc[2], desc[3][:])
-    desc_self[3][target_index] = ('LPCSTR', orig)
-    desc_call = (desc[0], desc[1], desc[2], desc[3][:])
-    desc_call[3][target_index] = (desc[3][target_index][0], orig + '_')
-    code = "WSTR %s(%s);\n" % (orig + '_', orig)
+    target_index = desc.index_arg(spec[0])
+    orig_type, orig_name = desc.parameter_types[target_index]
+    desc_self = desc.clone()
+    desc_self.parameter_types[target_index] = ('LPCSTR', orig_name)
+    desc_call = desc.clone()
+    desc_call.parameter_types[target_index] = (orig_type, orig_name + '_')
+    code = "WSTR %s(%s);\n" % (orig_name + '_', orig_name)
     return (desc_self, desc_call, code)
 
 spec = [
@@ -122,32 +159,32 @@ print 'Translation unit:', tu.spelling
 #dump(0, tu.cursor)
 for c in tu.cursor.get_children():
     if(c.type.kind.name == "FUNCTIONPROTO" and re.search('W$', c.spelling)):
-        desc = make_func_desc(c)
+        desc = FunctionDescriptor(c)
         processed = False
         for onespec in spec:
             flag = True
             for argspec in onespec[0]:
-                if index_arg(desc, argspec) == -1:
+                if desc.index_arg(argspec) == -1:
                     flag = False
                     break
             if(flag):
                 processed = True
-                outname = os.path.basename("%s" % desc[1])
+                outname = os.path.basename("%s" % desc.file)
                 (desc_self, desc_call, code) = onespec[1](desc, onespec[0])
-                desc_self = (desc_self[0][:-1] + 'U', desc_self[1], desc_self[2], desc_self[3])
+                desc_self.name = desc_self.name[:-1] + 'U'
                 cpp_header(outname)
                 with open(cpp_name(outname), 'a') as f:
-                    f.write(make_func_decl(desc_self) + "\n{\n")
+                    f.write(desc_self.make_func_decl() + "\n{\n")
                     f.write("\t" + code)
-                    f.write("\treturn " + make_func_call(desc_call) + ";\n}\n")
+                    f.write("\treturn " + desc_call.make_func_call() + ";\n}\n")
                 h_header(outname)
                 with open(h_name(outname), 'a') as f:
-                    f.write("extern " + make_func_decl(desc_self) + ";\n")
+                    f.write("extern " + desc_self.make_func_decl() + ";\n")
                 break
         if not processed:
-            outname = os.path.basename("%s" % desc[1])
+            outname = os.path.basename("%s" % desc.file)
             with open(txt_name(outname), 'a') as f:
-                f.write(make_func_decl(desc) + "\n")
+                f.write(desc.make_func_decl() + "\n")
 
 for actualname in h_status:
     with open(actualname, 'a') as f:
