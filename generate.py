c58e033498c9066db84cc6c428789ee4ae750495
Yak! <yak_ex@mx.scn.tv>
2013-10-08 13:02:21 +0900

    Add variants for read_only_wo_len (close gh-4).

--- generate.py
+++ generate.py
@@ -8,18 +8,32 @@ import clang.cindex
 from descriptor import FunctionDescriptor
 from dispatcher import Dispatcher
 
-def read_only_wo_len_imp(desc_self, desc_call, code, spec):
+def read_only_wo_len_imp(desc_self, desc_call, code, typespec):
     """
     """
-    target_index = desc.index_arg(spec[0]) # The first entry
+    target_index = desc.index_arg(typespec)
     orig_type, orig_name = desc.parameter_types[target_index]
     desc_self.parameter_types[target_index] = ('LPCSTR', orig_name)
     desc_call.parameter_types[target_index] = (orig_type, orig_name + '_')
-    code += "WSTR %s(%s);\n" % (orig_name + '_', orig_name)
+    code += "\tWSTR %s(%s);\n" % (orig_name + '_', orig_name)
     return (desc_self, desc_call, code)
 
-def read_only_wo_len(desc, spec):
-    return read_only_wo_len_imp(desc.clone(), desc.clone(), '', spec)
+def read_only_wo_len_idx(idx):
+    return lambda desc, typespecs: reduce( \
+        lambda acc, x: read_only_wo_len_imp(acc[0], acc[1], acc[2], x), \
+        map(lambda x: typespecs[x], idx), \
+        (desc.clone(), desc.clone(), '') \
+    )
+
+def read_only_wo_len_all(desc, typespecs):
+    return reduce( \
+        lambda acc, x: read_only_wo_len_imp(acc[0], acc[1], acc[2], x), \
+        typespecs, \
+        (desc.clone(), desc.clone(), '') \
+    )
+
+def read_only_wo_len(desc, typespecs):
+    return read_only_wo_len_imp(desc.clone(), desc.clone(), '', typespecs[0]) # for the first type spec
 
 # TODO: Need to complete parameter names
 
