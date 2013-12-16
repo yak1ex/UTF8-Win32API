5d83f4f1c7b00129aa0e30e1468775dd46870a06
Yak! <yak_ex@mx.scn.tv>
2013-12-16 19:21:14 +0900

    Avoid including stdio/iostream for wrong function pointers (ref gh-22).

--- helper/env.cpp
+++ helper/env.cpp
@@ -1,9 +1,7 @@
 #include <windows.h>
-#include "win32u_helper.hpp"
+#include "helper/win32u_helper.hpp"
 
-#include <iostream>
 #include <vector>
-#include <map>
 
 int _umain_stub()
 {
@@ -21,36 +19,6 @@ static int(*ptr_umain0)()                     = static_cast<int(*)()>(_umain);
 static int(*ptr_umain1)(int)                  = static_cast<int(*)(int)>(_umain);
 static int(*ptr_umain2)(int, char**)          = static_cast<int(*)(int, char**)>(_umain);
 static int(*ptr_umain3)(int, char**, char**)  = static_cast<int(*)(int, char**, char**)>(_umain);
-
-template<typename T>
-static inline unsigned long get_offset(T t)
-{
-	return reinterpret_cast<unsigned long>(_umain_stub) - reinterpret_cast<unsigned long>(t);
-}
-
-template<typename T>
-static inline void add_offset(T& t, unsigned long offset)
-{
-	t = reinterpret_cast<T>(reinterpret_cast<unsigned long>(t) + offset);
-}
-
-// GCC in StrawberryPerl 5.18.1.1 32bit seems to have a bug to set wrong addresses for function pointers.
-void fix_fptr()
-{
-	std::map<unsigned long, int> counter;
-	++counter[get_offset(ptr_umain)];
-	++counter[get_offset(ptr_umain0)];
-	++counter[get_offset(ptr_umain1)];
-	++counter[get_offset(ptr_umain2)];
-	++counter[get_offset(ptr_umain3)];
-//	assert(counter.size() == 2);
-	unsigned long offset = counter.begin()->second > (++(counter.begin()))->second ? counter.begin()->first : (++(counter.begin()))->first;
-	add_offset(ptr_umain, offset);
-	add_offset(ptr_umain0, offset);
-	add_offset(ptr_umain1, offset);
-	add_offset(ptr_umain2, offset);
-	add_offset(ptr_umain3, offset);
-}
 
 // LPSTR CreateEnvBlock(char cDelim);
 // void FreeEnvBlock(LPCSTR);
@@ -85,27 +53,20 @@ int main(void)
 	uargv.push_back(0);
 	// TODO: env
 
-	fix_fptr();
 	if(is_target(ptr_umain)) {
-		std::cout << "\"C\" _umain()" << std::endl;
 		return ptr_umain();
 	}
 	if(is_target(ptr_umain0)) {
-		std::cout << "_umain()" << std::endl;
 		return ptr_umain0();
 	}
 	if(is_target(ptr_umain1)) {
-		std::cout << "_umain(int)" << std::endl;
 		return ptr_umain1(argc);
 	}
 	if(is_target(ptr_umain2)) {
-		std::cout << "_umain(int, char**)" << std::endl;
 		return ptr_umain2(argc, &uargv[0]);
 	}
 	if(is_target(ptr_umain3)) {
-		std::cout << "_umain(int, char**, char**)" << std::endl;
 		return ptr_umain3(argc, &uargv[0], 0);
 	}
-	std::cout << "_umain not defined" << std::endl;
 	return -1; // Not reached
 }
